7e797750849face90749d0634848c1511f4991d6
nikko1231 <nikkoregulacion@gmail.com>
2025-04-28 08:26:37 -0800

    Initial commit with all project files

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB; 
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        DB::table('tbl_user')->truncate(); 
+    
+        $user = User::create([
+            'name' => 'nikko',
+            'email' => 'nikkoregulacion@gmail.com',
+            'password' => Hash::make('password123'),
+            'is_admin' => 1,
+        ]);
+    }
+}
